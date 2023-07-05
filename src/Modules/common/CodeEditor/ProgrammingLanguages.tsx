c58cefa9aaf10e89ffe35433826c822f51183edb
Nafil Arzzam <nafilarzzam19@gamil.com>
2023-07-05 15:07:06 +0530

    Styles update

---
+++ src/Modules/common/CodeEditor/ProgrammingLanguages.tsx
@@ -0,0 +1,22 @@
+export const ProgrammingLanguages = {
+  c: {
+    id: 50,
+    name: "C",
+  },
+  cpp: {
+    id: 54,
+    name: "C++",
+  },
+  java: {
+    id: 62,
+    name: "Java",
+  },
+  javaScript: {
+    id: 63,
+    name: "Javascript",
+  },
+  python: {
+    id: 71,
+    name: "Python",
+  },
+};
