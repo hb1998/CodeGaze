628dac0c48c5046e9a9b2824eec8e797ae210fc6
Nafil Arzzam <nafilarzzam19@gamil.com>
2023-07-06 14:39:25 +0530

    Separate Candidate Column

--- src/Modules/Candidate/Candidates.tsx
+++ src/Modules/Candidate/Candidates.tsx
@@ -3,6 +3,8 @@ import { Table } from "antd";
 import { CandidateAPIService } from "./services/Candidate.API";
 import Search from "antd/es/input/Search";
 import { Typography } from "antd";
+import { candidateColumn } from "./components/CandidateColumn";
+
 // import Main from "../common/CodeEditor/Main";
 
 const { Title } = Typography;
@@ -29,56 +31,6 @@ const Candidates = () => {
     fetchCandidates();
   }, []);
 
-  const formatTimestamp = (timestamp: string) => {
-    const date = new Date(timestamp);
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
-    const hours = date.getHours() % 12 || 12;
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    const ampm = date.getHours() >= 12 ? "PM" : "AM";
-
-    const formattedDate = `${day}/${month}/${year}`;
-    const formattedTime = `${hours}:${minutes}:${seconds} ${ampm}`;
-
-    return `${formattedDate} ${formattedTime}`;
-  };
-
-  const columns = [
-    {
-      title: "ID",
-      dataIndex: "id",
-      key: "id",
-    },
-    {
-      title: "Name",
-      dataIndex: "name",
-      key: "id",
-    },
-    {
-      title: "Email",
-      dataIndex: "emailId",
-      key: "id",
-    },
-    {
-      title: "Language",
-      dataIndex: ["assessment", 0, "language"],
-      key: "id",
-    },
-    {
-      title: "Status",
-      dataIndex: ["assessment", 0, "status"],
-      key: "id",
-    },
-    {
-      title: "Created At",
-      dataIndex: "created_at",
-      key: "id",
-      render: (timestamp: string) => formatTimestamp(timestamp),
-    },
-  ];
-
   return (
     <div style={{ padding: "10px" }}>
       <Title>Candidates</Title>
@@ -88,7 +40,7 @@ const Candidates = () => {
         onSearch={onSearch}
         enterButton
       />
-      <Table dataSource={candidates} columns={columns} />
+      <Table dataSource={candidates} columns={candidateColumn} />
     </div>
   );
   return <div>{/* <Main /> */}</div>;
