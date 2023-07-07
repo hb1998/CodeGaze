219e624b68ca3ea9443e653c98e882b50d26f2a9
Nafil Arzzam <nafilarzzam19@gamil.com>
2023-07-07 12:56:24 +0530

    feat(Main): selectedCodeEditorLanguage update and types add

--- src/Modules/common/CodeEditor/Main.tsx
+++ src/Modules/common/CodeEditor/Main.tsx
@@ -1,112 +1,95 @@
-import { useState } from "react";
-import Axios from "axios";
-// CodeMirror Languages Imports
-import { javascript } from "@codemirror/lang-javascript";
-import { python } from "@codemirror/lang-python";
-import { java } from "@codemirror/lang-java";
-import { cpp } from "@codemirror/lang-cpp";
+import { useState } from 'react';
+import Axios from 'axios';
 // SplitPane imports
-import SplitPane, { Pane } from "split-pane-react";
-import "split-pane-react/esm/themes/default.css";
+import SplitPane, { Pane } from 'split-pane-react';
+import 'split-pane-react/esm/themes/default.css';
 // Components Import
-import QuestionContent from "./QuestionContent";
-import CodeEditor from "./CodeEditor";
-import { ProgrammingLanguages } from "./ProgrammingLanguages";
-import Output from "./Output";
-import classes from "./Editor.module.css";
+import QuestionContent from './QuestionContent';
+import CodeEditor from './CodeEditor';
+import { ProgrammingLanguages } from './ProgrammingLanguages';
+import Output from './Output';
+import classes from './Editor.module.css';
+
+type ISelectedEditorLanguage = (typeof ProgrammingLanguages)[keyof typeof ProgrammingLanguages];
 
 const Editor = () => {
-  const [language, setLanguage] = useState("Javascript");
-  const [languageId, setLanguageId] = useState(63);
-  const [codeEditorLang, setCodeEditorLang] = useState(
-    javascript({ jsx: true })
-  );
-  const [code, setCode] = useState("");
-  const [output, setOutput] = useState("");
+    const [selectEditorLanguage, setSelectEditorLanguage] = useState<ISelectedEditorLanguage>(
+        ProgrammingLanguages.javaScript,
+    );
+    const [code, setCode] = useState('');
+    const [output, setOutput] = useState('');
 
-  const handleLanguageChange = (selectedLanguage: string) => {
-    setLanguage(selectedLanguage);
+    const handleLanguageChange = (
+        selectedLanguage: (typeof ProgrammingLanguages)[keyof typeof ProgrammingLanguages]['name'],
+    ) => {
+        switch (selectedLanguage) {
+            case 'Python':
+                setSelectEditorLanguage(ProgrammingLanguages.python);
+                break;
+            case 'Java':
+                setSelectEditorLanguage(ProgrammingLanguages.java);
+                break;
+            case 'C':
+                setSelectEditorLanguage(ProgrammingLanguages.c);
+                break;
+            case 'C++':
+                setSelectEditorLanguage(ProgrammingLanguages.cpp);
+                break;
+            default:
+                setSelectEditorLanguage(ProgrammingLanguages.javaScript);
+                break;
+        }
+    };
 
-    switch (selectedLanguage) {
-      case "Python":
-        setCodeEditorLang(python);
-        setLanguageId(ProgrammingLanguages.python.id);
-        break;
-      case "Java":
-        setCodeEditorLang(java);
-        setLanguageId(ProgrammingLanguages.java.id);
-        break;
-      case "C":
-        setCodeEditorLang(cpp);
-        setLanguageId(ProgrammingLanguages.c.id);
-        break;
-      case "C++":
-        setCodeEditorLang(cpp);
-        setLanguageId(ProgrammingLanguages.cpp.id);
-        break;
-      default:
-        setCodeEditorLang(javascript({ jsx: true }));
-        setLanguageId(ProgrammingLanguages.javaScript.id);
-        break;
-    }
-  };
+    const handleCodeChange = (value: string) => {
+        setCode(value);
+    };
 
-  const handleCodeChange = (value: string) => {
-    setCode(value);
-  };
+    const handleRun = () => {
+        Axios.post(process.env.compilerEndpoint || '', {
+            source_code: code,
+            language_id: selectEditorLanguage.id,
+        }).then((response) => {
+            setOutput(response.data.stdout);
+            console.log(response.data);
+        });
+    };
 
-  const handleRun = () => {
-    Axios.post(
-      "http://localhost:2358/submissions/?base64_encoded=false&wait=true",
-      {
-        source_code: code,
-        language_id: languageId,
-      }
-    ).then((response) => {
-      setOutput(response.data.stdout);
-      console.log(response.data);
-    });
-  };
+    const handleReset = () => {
+        setCode('');
+        setOutput('');
+    };
 
-  const handleReset = () => {
-    setCode("");
-    setOutput("");
-  };
+    const handleSubmit = () => {
+        console.log('Submitted code:', code);
+        console.log('Selected language:', selectEditorLanguage.name);
+    };
+    const [sizes, setSizes] = useState([300, '100%', '25%']);
 
-  const handleSubmit = () => {
-    console.log("Submitted code:", code);
-    console.log("Selected language:", language);
-  };
-  const [sizes, setSizes] = useState([300, "100%", "25%"]);
-
-  return (
-    <div>
-      <div className={classes.main}>
-        <SplitPane split="vertical" sizes={sizes} onChange={setSizes}>
-          <Pane>
-            <QuestionContent />
-          </Pane>
-          <Pane minSize="50%" maxSize="80%" className={classes.pane}>
-            <CodeEditor
-              language={language}
-              handleLanguageChange={handleLanguageChange}
-              handleReset={handleReset}
-              code={code}
-              codeEditorLang={codeEditorLang}
-              handleCodeChange={handleCodeChange}
-            />
-          </Pane>
-          <Pane>
-            <Output
-              output={output}
-              handleRun={handleRun}
-              handleSubmit={handleSubmit}
-            />
-          </Pane>
-        </SplitPane>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div className={classes.main}>
+                <SplitPane split="vertical" sizes={sizes} onChange={setSizes}>
+                    <Pane>
+                        <QuestionContent />
+                    </Pane>
+                    <Pane minSize="50%" maxSize="80%" style={{ margin: '2px' }}>
+                        <CodeEditor
+                            languageName={selectEditorLanguage.name}
+                            handleLanguageChange={handleLanguageChange}
+                            handleReset={handleReset}
+                            code={code}
+                            codeEditorLang={selectEditorLanguage.lang}
+                            handleCodeChange={handleCodeChange}
+                        />
+                    </Pane>
+                    <Pane>
+                        <Output output={output} handleRun={handleRun} handleSubmit={handleSubmit} />
+                    </Pane>
+                </SplitPane>
+            </div>
+        </div>
+    );
 };
 
 export default Editor;
