1501ea0824c069b491940a1afa5caebe5439fdd3
vikramravi13 <124550630+vikramravi13@users.noreply.github.com>
2023-07-07 10:16:05 +0530

    Revert "feat(codeScrutinex): basic flowfor assessment"
    This reverts commit 409d24f24e99b93371671783c3c99ce586b016b4.

--- src/Modules/Assessments/Open/OpenAssessment.tsx
+++ src/Modules/Assessments/Open/OpenAssessment.tsx
@@ -5,43 +5,36 @@ import {
 } from "@ant-design/icons";
 import { Button, Tabs } from "antd";
 import TabPane from "antd/es/tabs/TabPane";
-import { Link } from "react-router-dom";
 
 const OpenAssessment = () => {
   return (
     <div>
       <Button icon={<ArrowLeftOutlined />} type="link">
         {" "}
-       <Link to="/assessments/open"> back to candidate results</Link>
+        back to all assessments
       </Button>
-      
-     <div  style={{ float: "right" ,display: 'flex', padding: '10px' }}>
-      <Button 
-      type="link"
+     
+      <Button
         icon={<CopyOutlined />}
         // style={{ float: "right", padding: "5px" }}
       >
         Duplicate
       </Button>
       <Button
-      type="link"
         icon={<LaptopOutlined />}
         // style={{ float: "right", padding: "5px" }}
       >
         Preview
       </Button>
-      <Button type="primary">
+      <Button type="primary" style={{ float: "right", padding: "5px" }}>
         
         save
       </Button>
-      </div>
 
       <div>
         <Tabs>
-          <TabPane tab="Challenges" key="1">
-          <Link to="challengeAssessment">Challenges</Link></TabPane>
-          <TabPane tab="Settings" key="2">
-            <Link to="challengeSettings">Settings</Link></TabPane>
+          <TabPane tab="Challenges">Challenges</TabPane>
+          <TabPane tab="Settings">Settings</TabPane>
         </Tabs>
       </div>
     </div>
