6b13a6ad553e399b4826210fb8223349dc92a4b5
Nafil Arzzam <nafilarzzam19@gamil.com>
2023-07-10 14:52:17 +0530

    fix(editor): changes in editor - compiler error render - type add - styles change in code editor

--- src/Modules/common/CodeEditor/CodeEditor.tsx
+++ src/Modules/common/CodeEditor/CodeEditor.tsx
@@ -5,50 +5,43 @@ import { ProgrammingLanguages } from './ProgrammingLanguages';
 
 import classes from './Editor.module.css';
 
-const { Text } = Typography;
-
-interface ICodeEditorProps {
-    languageName: string;
-    handleLanguageChange: (lang: (typeof ProgrammingLanguages)[keyof typeof ProgrammingLanguages]['name']) => void;
-    handleReset: () => void;
-    code: string;
-    codeEditorLang: any;
-    handleCodeChange: (value: string) => void;
-}
+type LanguageName = (typeof ProgrammingLanguages)[keyof typeof ProgrammingLanguages]['name'];
 
 const options = Object.entries(ProgrammingLanguages).map(([key, value]) => ({
     label: value.name,
     value: value.name,
 }));
 
+interface ICodeEditorProps {
+    languageName: string;
+    handleLanguageChange: (lang: LanguageName) => void;
+    handleReset: () => void;
+    code: string;
+    codeEditorLang: any;
+    handleCodeChange: (value: string) => void;
+}
+
 const CodeEditor = (props: ICodeEditorProps) => {
     return (
-        <div className={`${classes.content} ${classes.pane2}`} style={{ padding: '1rem', marginRight: '0.5rem' }}>
+        <div className={`${classes.content} ${classes.pane2}`}>
             <div className={classes.editorHeader}>
-                <div>
-                    <Text strong style={{ paddingRight: '1rem' }}>
-                        Select Language:
-                    </Text>
-                    <Select
-                        value={props.languageName}
-                        onChange={(value) =>
-                            props.handleLanguageChange(value as 'C' | 'C++' | 'Java' | 'Javascript' | 'Python')
-                        }
-                        style={{ width: '7rem' }}
-                        options={options}
-                    />
-                </div>
+                <Select
+                    value={props.languageName}
+                    onChange={(value) => props.handleLanguageChange(value as LanguageName)}
+                    style={{ width: '7rem' }}
+                    options={options}
+                />
                 <Button type="primary" onClick={props.handleReset}>
                     Reset
                 </Button>
             </div>
             <CodeMirror
                 value={props.code}
-                minHeight="100vh"
+                height="70vh"
                 theme="dark"
                 extensions={[props.codeEditorLang]}
                 onChange={props.handleCodeChange}
-                style={{ borderRadius: '1rem' }}
+                style={{ padding: '0.5rem 1rem', overflow: 'auto' }}
             />
         </div>
     );
