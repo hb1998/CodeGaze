588cfd5c1a0c48bf8fd40b51ef072918f7def69b
vikramravi13 <124550630+vikramravi13@users.noreply.github.com>
2023-07-18 22:01:08 +0530

    feat(codescrutinex): added funtionality in exam

--- src/Modules/Exam/Exam.tsx
+++ src/Modules/Exam/Exam.tsx
@@ -3,21 +3,18 @@ import Title from 'antd/es/typography/Title';
 import { Outlet, useNavigate } from 'react-router-dom';
 import './styles/Exam.css'
 
-const { TabPane } = Tabs;
+// const { TabPane } = Tabs;
 const { Content } = Layout;
 
 const Exam = () => {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     return (
         <Layout>
             <header>
                 <Title>Exam</Title>
                 <div style={{ padding: '5px' }}>
-                    <Tabs defaultActiveKey="open" onTabClick={(key) => navigate(key)}>
-                        <TabPane tab="Open" tabKey="open" key="open"></TabPane>
-                        <TabPane tab="Analytics" tabKey="analytics" key="analytics"></TabPane>
-                    </Tabs>
+                
                     <Content>
                         <Outlet />
                     </Content>
--- src/Modules/Exam/Open/OpenAssessment.tsx
+++ src/Modules/Exam/Open/OpenAssessment.tsx
@@ -1,36 +1,52 @@
-import { ArrowLeftOutlined, CopyOutlined, LaptopOutlined } from '@ant-design/icons';
+import { ArrowLeftOutlined, CopyOutlined,  SelectOutlined } from '@ant-design/icons';
 import { Button, Tabs } from 'antd';
 import TabPane from 'antd/es/tabs/TabPane';
 import { Link } from 'react-router-dom';
 import ChallengesAssessment from './ChallengesAssessment';
 import SettingsAssessments from './SettingsAssessment';
+import { useState } from 'react';
 
 
 const OpenAssessment = () => {
+    const [challengesChanges, setChallengesChanges] = useState(null);
+    const [settingsChanges, setSettingsChanges] = useState(null);
+
+    const handleChallengesChange = (changes) => {
+        setChallengesChanges(changes);
+    };
+
+    const handleSettingsChange = (changes) => {
+        setSettingsChanges(changes);
+    };
+
+    const handleSave = () => {
+        
+        console.log('Saving changes:', challengesChanges, settingsChanges);
+    };
     return (
-        <div >
-            <Button icon={<ArrowLeftOutlined />} type="link">
-                <Link to="/assessments/open"> back to candidate results</Link>
-            </Button>
+        <div style={{ padding: '0px 0px' }} >
+           <Link to="/assessments/open"> <Button  type="link" icon={<ArrowLeftOutlined />} ></Button>  back to candidate results</Link>
+            
+                
 
             <div style={{ float: 'right', display: 'flex', padding: '10px' }}>
                 <Button type="link" icon={<CopyOutlined />}>
                     Duplicate
                 </Button>
-                <Button type="link" icon={<LaptopOutlined />}>
+                <Button type="link" icon={<SelectOutlined />}>
                     Preview
                 </Button>
-                <Button type="primary">save</Button>
+                <Button type="primary"  onClick={handleSave}>save</Button>
             </div>
-            <div> <h1>Technical assessment</h1></div>
+            <div  style={{ padding: '18px' }}> <h1>Technical assessment</h1></div>
             <div>
                 <Tabs style={{ padding: '18px' }}>
                     <TabPane tab="Challenges" key="1">
-                        <ChallengesAssessment />
+                        <ChallengesAssessment onChange={handleChallengesChange}/>
                     </TabPane>
                  
                     <TabPane tab="Settings" key="2">
-                        <SettingsAssessments/>
+                        <SettingsAssessments onChange={handleSettingsChange}/>
                     </TabPane>
                 </Tabs>
             </div>
