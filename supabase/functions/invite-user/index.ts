c3d38a1db40363d788027cb7fb822885cbea4f41
hb1998 <rhabeeb1998@gmail.com>
2023-07-20 23:30:12 +0530

    feat(functions): added requried supabase edge functions

---
+++ supabase/functions/invite-user/index.ts
@@ -0,0 +1,39 @@
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+
+serve(async (req) => {
+
+  const { emailId } = await req.json();
+  const supabase = createClient(
+    Deno.env.get('SUPABASE_URL'),
+    Deno.env.get('SUPABASE_SERVICE_ROLE_KEY'))
+
+  try {
+
+    const { data, error } = await supabase.auth.admin.generateLink({
+      type: 'invite',
+      email: emailId,
+    });
+
+    if (error) {
+      throw error;
+    }
+
+    return new Response(
+      JSON.stringify(data),
+      { headers: { "Content-Type": "application/json" } },
+    )
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { 'Content-Type': 'application/json' },
+      status: 400,
+    })
+  }
+})
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
