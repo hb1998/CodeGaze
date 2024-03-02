a7940012655e81a6b07ae8e5b93db8533273054e
Habeeb <rhabeeb1998@gmail.com>
2024-03-02 11:19:31 +0530

    feat: candidate invite initial commit

--- src/Modules/Exam/ExamList.tsx
+++ src/Modules/Exam/ExamList.tsx
@@ -1,15 +1,26 @@
 import { useEffect, useState } from 'react';
 import { CopyOutlined, PlusCircleFilled } from '@ant-design/icons';
 import dayjs from 'dayjs';
-import { Button, Card, Col, Divider, Input, Row, Skeleton, Statistic, Tag } from 'antd';
+import { Button, Card, Col, Divider, Input, Row, Skeleton, Statistic, Tag, Typography, Space, Form, Modal } from 'antd';
 import { Content } from 'antd/es/layout/layout';
 import { Outlet, useNavigate } from 'react-router';
 import Title from 'antd/es/typography/Title';
 import { useSelector } from 'react-redux';
 import { IRootState } from '../../store';
 import { ExamAPIService } from './services/Exam.API';
+import { supabase } from '../API/supabase';
 import { toast } from 'react-toastify';
 import { ROUTES } from '../../constants/Route.constants';
+import { Candidate, CandidateInsertDto } from '../../types/Models';
+import { FunctionsHttpError } from '@supabase/supabase-js';
+import { FUNCTIONS } from '../../constants/functions.constants';
+
+const { Text } = Typography;
+
+interface FormValues {
+    name: string;
+    email: string;
+}
 
 export type ExamQueryResult = Awaited<ReturnType<typeof ExamAPIService.getAll>>;
 
@@ -19,6 +30,13 @@ const ExamList = () => {
     const [loading, setLoading] = useState<boolean>(true);
     const [newExamLoading, setNewExamLoading] = useState<boolean>(false);
     const [searchQuery, setSearchQuery] = useState('');
+
+    const [modalVisible, setModalVisible] = useState(false);
+    const [modalLoading, setModalLoading] = useState(false);
+    const [inviteLink, setInviteLink] = useState('');
+    const [examId, setExamId] = useState('');
+    const [form] = Form.useForm();
+
     const navigate = useNavigate();
 
     const fetchExams = async () => {
@@ -90,6 +108,52 @@ const ExamList = () => {
         toast.success('Link copied to clipboard');
     };
 
+    const copyCandidateInviteLink = () => {
+        navigator.clipboard.writeText(inviteLink);
+        toast.success('Candidate Invite Link copied to clipboard');
+    };
+
+    const openGenerateLinkPopup = (examId) => {
+        setModalVisible(true);
+        setExamId(examId);
+    };
+
+    const onSubmit = (values: FormValues) => {
+        setModalLoading(true);
+        const userData = {
+            emailId: values.email,
+            name: values.name,
+        };
+        createCandidate(userData)
+            .then((candidateData: Candidate) => {
+                setModalLoading(false);
+                const domain = window.location.origin;
+                const link = `${domain}${ROUTES.CANDIDATE_ASSESSMENT}/${examId}/${candidateData.id}`;
+                setInviteLink(link);
+            })
+            .catch((error) => {
+                setModalLoading(false);
+                console.error('Error creating user:', error);
+                toast.error(error.message ?? 'Error creating user');
+            });
+    };
+
+    const createCandidate = async (candidateData: CandidateInsertDto) => {
+        const { data, error } = await supabase.functions.invoke<CandidateInsertDto>(FUNCTIONS.CREATE_CANDIDATE, {
+            body: candidateData,
+        });
+        setModalLoading(false);
+        if (error) {
+            if (error instanceof FunctionsHttpError) {
+                const errorMessage = await error.context.json();
+                throw new Error(errorMessage.error);
+            } else {
+                throw error;
+            }
+        }
+        return data;
+    };
+
     const handleOpenCard = (exam: ExamQueryResult[number]) => {
         navigate(`/assessments/open/openAssessment/${exam.id}`, {
             state: {
@@ -117,6 +181,66 @@ const ExamList = () => {
             >
                 New Exam
             </Button>
+            <Modal
+                title="Generate Candidate Invite Link"
+                open={modalVisible}
+                okText="Invite user"
+                onCancel={() => {
+                    form.resetFields();
+                    setModalVisible(false);
+                    setInviteLink('');
+                }}
+                afterClose={() => {
+                    form.resetFields();
+                    setInviteLink('');
+                }}
+                footer={null}
+            >
+                <Form form={form} onFinish={onSubmit}>
+                    <Form.Item label="Name" name="name" rules={[{ required: true, message: 'Please enter your name' }]}>
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[
+                            { required: true, message: 'Please enter your email address' },
+                            { type: 'email', message: 'Please enter a valid email address' },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    {inviteLink && (
+                        <div style={{ marginBottom: '1rem' }}>
+                            <Text code>{inviteLink}</Text>
+                            <CopyOutlined onClick={copyCandidateInviteLink} />
+                        </div>
+                    )}
+
+                    <Form.Item>
+                        <Button
+                            loading={modalLoading}
+                            disabled={modalLoading || !!inviteLink}
+                            type="primary"
+                            htmlType="submit"
+                            style={{ marginRight: '8px' }}
+                        >
+                            Generate Invite Link
+                        </Button>
+                        <Button
+                            type="default"
+                            onClick={() => {
+                                form.resetFields();
+                                setModalVisible(false);
+                            }}
+                        >
+                            Close
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Modal>
             <div>
                 {loading ? (
                     <Skeleton />
@@ -160,18 +284,21 @@ const ExamList = () => {
                                         </Row>
                                         <Divider></Divider>
                                         <Row className="actions-container">
-                                            <Col>
+                                            <Space>
                                                 <Button onClick={() => copyInviteLink(exam.id)} type="dashed">
                                                     Copy Invite Link
                                                 </Button>
-                                                <Button
+                                                <Button onClick={() => openGenerateLinkPopup(exam.id)} type="link">
+                                                    Generate Link
+                                                </Button>
+                                                {/* <Button
                                                     onClick={() => handleDuplicate(exam.id)}
                                                     type="link"
                                                     icon={<CopyOutlined />}
                                                 >
                                                     Duplicate
-                                                </Button>
-                                            </Col>
+                                                </Button> */}
+                                            </Space>
                                             <Col style={{ display: 'flex', alignItems: 'center' }}>
                                                 <Tag>{exam.created_by}</Tag> |{' '}
                                                 {dayjs(exam.created_at).format('MMM DD YYYY')}
