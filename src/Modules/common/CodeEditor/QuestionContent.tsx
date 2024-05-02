0f6634b64c9b2a236f386d4cc391d15a818d3b89
Habeeb <rhabeeb1998@gmail.com>
2024-05-02 22:29:05 +0530

    feat: added question desc to report page

--- src/Modules/common/CodeEditor/QuestionContent.tsx
+++ src/Modules/common/CodeEditor/QuestionContent.tsx
@@ -2,18 +2,30 @@ import MDEditor from '@uiw/react-md-editor';
 import classes from './Editor.module.css';
 import { Challenge } from '../../../types/Models';
 import Title from 'antd/es/typography/Title';
+import React from 'react';
 
-const QuestionContent = ({ challenge }: { challenge: Challenge }) => {
+const QuestionContent = ({
+    challenge,
+    hideTitle = false,
+    editorStyles = {},
+}: {
+    challenge: Pick<Challenge,'description' | 'name'>;
+    hideTitle?: boolean;
+    editorStyles?: React.CSSProperties;
+}) => {
     return (
         <div className={`${classes.content} ${classes.pane1}`}>
-            <Title style={{ margin: '0.5rem 0' }} level={5}>
-                {challenge?.name}
-            </Title>
+            {!hideTitle && (
+                <Title style={{ margin: '0.5rem 0' }} level={5}>
+                    {challenge?.name}
+                </Title>
+            )}
             <MDEditor
                 value={challenge?.description}
                 hideToolbar={true}
                 preview={'preview'}
-                height={'calc(100% - 30px)'}
+                style={editorStyles}
+                height={editorStyles.height}
             />
         </div>
     );
